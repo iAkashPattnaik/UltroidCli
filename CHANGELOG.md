bc635a3665b47df3edd4d38a8555d68669396516
Akash Pattnaik <akashjio66666@gmail.com>
2021-08-12 12:56:15 +0530

    add changelog and env command

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+## 1.0.1
+ - Add `env` option
+ - Add `CHANGELOG.md` file
+ - Add `docs` directory [Markdown]
+ - Fix `upgrade` command
+ - Add UltroidCli version check in `try/catch` block to avoid internet failure errors
+
+## 1.0.0
+ - Initial release
---
+++ lib/env.dart
@@ -0,0 +1,42 @@
+// UltroidCli
+// Copyright (C) 2021 Akash Pattnaik
+//
+// This file is a part of < https://github.com/BLUE-DEVIL1134/UltroidCli/ >
+// PLease read the GNU Affero General Public License in
+// <https://www.github.com/BLUE-DEVIL1134/UltroidCli/blob/main/LICENSE/>.
+
+import 'package:dart_dotenv/dart_dotenv.dart';
+import 'package:colorize/colorize.dart';
+
+void dotEnvController(List<String> cliArguments) {
+  final filePath = './TeamUltroid/.env';
+  final dotEnv = DotEnv(filePath: filePath);
+  if (!dotEnv.exists()) {
+    print(Colorize('Env File Not Found ! [ ${Colorize(filePath).lightGreen()} ${Colorize("]").lightRed()}').lightRed());
+  }
+  if (cliArguments.length < 2 && cliArguments[0].contains('.')) {
+    return print(Colorize('Usage: ${Colorize("ultroid env.<key> <value>").cyan()}').lightRed());
+  }
+  if (cliArguments[0].split('.')[1] == '') {
+    return print(Colorize('Error: Invalid Key Supplied !').lightRed());
+  }
+  switch (cliArguments[0].split('.')[1]) {
+    case 'create':
+      print(['ultroid', 'env.create', 'new']);
+      dotEnv.createNew(recrusive: true);
+      print(Colorize('New Env File Created ! [ ${Colorize(filePath).lightGreen()} ${Colorize("]").cyan()}').cyan());
+      break;
+    case 'get':
+      print(['ultroid', 'env.get', 'all']);
+      dotEnv.getDotEnv().forEach((String key, String value) {
+        print(Colorize(key).lightMagenta().toString() + ' -> ' + Colorize(value).lightGreen().toString());
+      });
+      break;
+    default:
+      print(['ultroid', cliArguments[0], cliArguments[1]]);
+      dotEnv.getDotEnv();
+      dotEnv.set(cliArguments[0].split('.')[1], cliArguments[1]);
+      dotEnv.saveDotEnv();
+      print(Colorize('Key "${Colorize(cliArguments[0].split('.')[1]).green()}${Colorize('"').cyan()} ${Colorize("has been saved to [ ${Colorize(filePath).lightMagenta()}").cyan().default_slyle()} ${Colorize("]").cyan()}').cyan());
+  }
+}
